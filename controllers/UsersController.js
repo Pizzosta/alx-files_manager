7c64009eb3d8e8336648cbcc1a3f63bedbd3d69c
pizzosta <pizzostor@gmail.com>
2023-10-07 01:35:54 +0100

    Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,51 +1,46 @@
 import sha1 from 'sha1';
+// import { ObjectID } from 'mongodb';
+import Queue from 'bull';
 import dbClient from '../utils/db';
+// import redisClient from '../utils/redis';
 
 const db = dbClient.client.db(); // Get the MongoDB database instance
+const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
 
 class UsersController {
-  static async postNew(req, res) {
-    try {
-      const { email, password } = req.body;
+  static postNew(request, response) {
+    console.log('DBKLIENT', dbClient);
+    console.log('Database Name:', db.databaseName);
 
-      // Check if email and password are provided
-      if (!email) {
-        return res.status(400).json({ error: 'Missing email' });
-      }
+    const { email } = request.body;
+    const { password } = request.body;
 
-      if (!password) {
-        return res.status(400).json({ error: 'Missing password' });
-      }
-
-      // Check if the email already exists in the DB
-      const existingUser = await db.collection('users').findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ error: 'Already exists' });
-      }
-
-      // Hash the password using SHA1
-      const hashedPassword = sha1(password);
-
-      // Create a new user object
-      const newUser = {
-        email,
-        password: hashedPassword,
-      };
-
-      // Insert the new user into the 'users' collection
-      const result = await db.collection('users').insertOne(newUser);
-
-      // Return the newly created user with minimal information
-      const createdUser = {
-        id: result.insertedId,
-        email: newUser.email,
-      };
-
-      return res.status(201).json(createdUser);
-    } catch (error) {
-      console.error('Error in postNew:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+    if (!email) {
+      response.status(400).json({ error: 'Missing email' });
+      return;
     }
+    if (!password) {
+      response.status(400).json({ error: 'Missing password' });
+      return;
+    }
+
+    const users = db.collection('users');
+    users.findOne({ email }, (err, user) => {
+      if (user) {
+        response.status(400).json({ error: 'Already exist' });
+      } else {
+        const hashedPassword = sha1(password);
+        users.insertOne(
+          {
+            email,
+            password: hashedPassword,
+          },
+        ).then((result) => {
+          response.status(201).json({ id: result.insertedId, email });
+          userQueue.add({ userId: result.insertedId });
+        }).catch((error) => console.log(error));
+      }
+    });
   }
 }
 
